823c6d0e2acc449703eb49ad2780f9557882a9df
R-Rijnbeek <robert270384@gmail.com>
2022-03-23 16:38:11 +0100

    finished improving build

--- build.bat
+++ build.bat
@@ -1,1 +1,27 @@
+if not exist ".\.vscode\" mkdir .\.vscode
+
+( 
+    echo {
+    echo     // Use IntelliSense to learn about possible attributes.
+    echo     // Hover to view descriptions of existing attributes.
+    echo     // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
+    echo     "version": "0.2.0",
+    echo     "configurations": [
+    echo         {
+    echo             "name": "Python: Archivo actual",
+    echo             "type": "python",
+    echo             "request": "launch",
+    echo             "program": "${file}",
+    echo             "console": "integratedTerminal"
+    echo         }
+    echo     ]
+    echo }
+) > .\.vscode\launch.json
+
+( 
+    echo {
+    echo     "python.defaultInterpreterPath": ".\\env\\python.exe",
+    echo }
+) > .\.vscode\settings.json
+
 conda env create -f environment.yml --prefix env
