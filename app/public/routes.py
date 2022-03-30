0c5df4da539e752b70301286b75d4930f08674bd
R-Rijnbeek <robert270384@gmail.com>
2022-03-31 01:48:49 +0200

    bug solved related testing it

--- app/public/routes.py
+++ app/public/routes.py
@@ -49,7 +49,7 @@ def get_js():
 	try:	
 		file_name = request.args["ifc"]
 		filename, file_extension = splitext(file_name)
-		if file_extension.replaceee(".", "") in current_app.config["UPLOADED_EXTENSIONS"]:
+		if file_extension.replace(".", "") in current_app.config["UPLOADED_EXTENSIONS"]:
 			shape_path = current_app.config["SHAPE_DIR"]
 			ifc_file = ifcopenshell.open(join(current_app.config["BASE_DIR"],"app","static","ifc",file_name))
 			my_ren = ThreejsRenderer(path = shape_path )
